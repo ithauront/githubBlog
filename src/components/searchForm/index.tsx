8ee66a6ce7881a97cbe089e4b119aadcc84d6e97
IURI <123806396+ithauront@users.noreply.github.com>
2023-07-02 17:08:41 +0200

    searchform api
    made the mogic so that the searchform uses github api to look for issues on my repository

--- src/components/searchForm/index.tsx
+++ src/components/searchForm/index.tsx
@@ -1,9 +1,45 @@
 import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFromInput = z.infer<typeof searchFormSchema>
+
+type Issue = {
+  title: string
+  text: string
+}
 
 export function SearchForm() {
+  const [issues, setIssues] = useState<Issue[]>([])
+  const { register, handleSubmit, getValues } = useForm<SearchFromInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  const fetchIssues = async () => {
+    try {
+      const { query } = getValues()
+      const response = await axios.get(
+        `https://api.github.com/search/issues?q=${query}%20repo:iThauront/githubBlog`,
+      )
+      setIssues(response.data.items)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    console.log(issues)
+  }, [issues])
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Buscar conteúdo" />
+    <SearchFormContainer onSubmit={handleSubmit(fetchIssues)}>
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </SearchFormContainer>
   )
 }
